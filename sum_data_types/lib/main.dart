bc4f9b9c61cf0b09c77f9000d6b9136a5322c1f6
Stefan Wehr <wehr@cp-med.com>
2019-10-15 11:12:11 +0200

    annotations for sum types

--- sum_data_types/lib/main.dart
+++ sum_data_types/lib/main.dart
@@ -2,7 +2,43 @@ export 'package:meta/meta.dart' show immutable, required;
 
 import 'package:meta/meta.dart';
 
+//
+// Data classes
+//
 @immutable
 class DataClass {
   const DataClass();
+}
+
+//
+// Sum types
+//
+@immutable
+class SumType {
+  const SumType(this.cases);
+
+  final List<CaseBase> cases;
+}
+
+@immutable
+abstract class CaseBase {
+  String get name;
+}
+
+@immutable
+class Case<T> implements CaseBase {
+  const Case({this.name});
+
+  @override
+  final String name;
+}
+
+@immutable
+class CaseT implements CaseBase {
+  const CaseT(this.typeParameterIndex, {this.name});
+
+  final int typeParameterIndex;
+
+  @override
+  final String name;
 }

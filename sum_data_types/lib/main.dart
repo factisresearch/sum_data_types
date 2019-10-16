e0900c09ed27a172b0639dabea6320575adf57bc
Stefan Wehr <wehr@cp-med.com>
2019-10-16 11:43:34 +0200

    prepare for new way of defining sum types

--- sum_data_types/lib/main.dart
+++ sum_data_types/lib/main.dart
@@ -15,32 +15,7 @@ class DataClass {
 //
 @immutable
 class SumType {
-  const SumType(this.cases);
-
-  final List<CaseBase> cases;
-}
-
-@immutable
-abstract class CaseBase {
-  String get name;
-}
-
-@immutable
-class Case<T> implements CaseBase {
-  const Case({this.name});
-
-  @override
-  final String name;
-}
-
-@immutable
-class CaseT implements CaseBase {
-  const CaseT(this.typeParameterIndex, {this.name});
-
-  final int typeParameterIndex;
-
-  @override
-  final String name;
+  const SumType();
 }
 
 @immutable
